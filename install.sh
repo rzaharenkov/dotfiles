bd0e6c726f7459c7c9c1b70cb1f3d3b0ddf3c2e3
Raman Zakharenkau <ZaharenkovRoman@gmail.com>
2021-12-06 10:45:53 -0600

    Add iterm

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+if [[ -d /Applications/iTerm.app ]]; then
+  ln -sf ~/dotfiles/.iterm ~/.iterm
+
+  defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/.iterm"
+  defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
+fi
+
